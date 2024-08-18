86084003d332594d74a6842bfc6af89a630290d6
rajikrishnamoorthy <30640789+rajikrishnamoorthy@users.noreply.github.com>
2024-08-18 14:11:10 +0530

    Update bedrockBackend.py

--- streamlit/bedrockBackend.py
+++ streamlit/bedrockBackend.py
@@ -1,1 +1,32 @@
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain_aws import ChatBedrock
+#2a Write a function for invoking model- client connection with Bedrock with profile, model_id & Inference params- model_kwargs
+def demo_chatbot():
+    demo_llm=ChatBedrock(
+       credentials_profile_name='genailearner',
+       region_name='us-east-1',
+       model_id='anthropic.claude-3-haiku-20240307-v1:0',
+       model_kwargs= {
+           "max_tokens": 300,
+           "temperature": 0.1,
+           "top_p": 0.9,
+           "stop_sequences": ["\n\nHuman:"]} )
+    return demo_llm
+
+
+def demo_memory():
+    llm_data=demo_chatbot()
+    memory=ConversationSummaryBufferMemory(llm=llm_data,max_token_limit=300)
+    return memory
+
+#4 Create a Function for Conversation Chain - Input text + Memory
+def demo_conversation(input_text,memory):
+    llm_chain_data=demo_chatbot()
+    llm_conversation=ConversationChain(llm=llm_chain_data,memory=memory,verbose=True)
+
+#5 Chat response using invoke (Prompt template)
+    chat_reply=llm_conversation.invoke(input_text)
+    return chat_reply['response']
+
 
